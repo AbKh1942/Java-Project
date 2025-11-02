f181c9b34153a68f67c774283fee55b112227b8a
Alex <Bahno>
2025-11-02 18:19:31 +0100

    Initial commit

---
+++ TrafficSim/src/main/java/de/uni/trafficsim/App.java
@@ -0,0 +1,14 @@
+package de.uni.trafficsim;
+
+import org.eclipse.sumo.libtraci.Simulation;
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
